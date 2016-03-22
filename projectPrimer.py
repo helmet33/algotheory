c99107ac64bbe1c6e82e31bb3140701ebe2ce306
Garrett Jordan <garrettjor@gmail.com>
2016-03-22 03:36:42 +0000

    Limit length of dictionary word
    Limit dictionary to words less then or equal to 9

--- projectPrimer.py
+++ projectPrimer.py
@@ -11,6 +11,8 @@ for x in allwords:
     x = x.rstrip()
     words.append(x)
 
+words = [word for word in words if len(word) <= 9 ]
+
 # Convert list of words to set to ensure no duplicates
 words = set(words)
 
