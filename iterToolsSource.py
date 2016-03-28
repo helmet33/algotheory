4dbfafcbab8fa2f74d2fda703fecd29c620e4490
helmet33 <garrettjor@gmail.com>
2016-03-28 17:28:58 +0100

    Add some files for discussion
    Added source of itertools marked up and printing and another permutation
    method to compare.

--- iterToolsSource.py
+++ iterToolsSource.py
@@ -1,1 +1,32 @@
-# look up itertools source and permutation cycles 
+# look up itertools source and permutation cycles
+# Analysis of the itertools permutatioon function
+# As far as I can see it is using permutation cyles to 
+# Work out the permtations and generates the results 
+def permutations(iterable, r=None):
+    # permutations('ABCD', 2) --> AB AC AD BA BC BD CA CB CD DA DB DC
+    # permutations(range(3)) --> 012 021 102 120 201 210
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r
+    if r > n:
+        return
+    indices = list(range(n))
+    cycles = list(range(n, n-r, -1))
+    print(cycles)
+    yield tuple(pool[i] for i in indices[:r])
+    while n:
+        for i in reversed(range(r)):
+            cycles[i] -= 1
+            if cycles[i] == 0:
+                indices[i:] = indices[i+1:] + indices[i:i+1]
+                cycles[i] = n - i
+            else:
+                j = cycles[i]
+                indices[i], indices[-j] = indices[-j], indices[i]
+                yield tuple(pool[i] for i in indices[:r])
+                break
+        else:
+            return
+
+a = list(permutations([0, 1, 2 ,3], 3))
+
---
+++ myPerms.py
@@ -0,0 +1,12 @@
+# My permutations, from a conversation at 
+# http://stackoverflow.com/questions/8683092/calculating-combinations-of-length-k-from-a-list-of-length-n-using-recursion
+
+def permute(word, n):
+	# if we reach zero return empty list
+    if n==0: yield []
+    else:
+    	# traverse word length
+        for i in range(len(word)):
+        	# traverse rest of word and recur
+            for combo in permute(word[i+1:],n-1):
+                yield [word[i]]+combo
