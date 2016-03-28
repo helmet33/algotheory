d88460d4a767aa7651344bb1975db267226e5b63
helmet33 <garrettjor@gmail.com>
2016-03-28 21:26:33 +0100

    tidy up filestuff

--- fileStuff.py
+++ fileStuff.py
@@ -1,18 +1,18 @@
 # file stuff
 dictionary = set()
 def genDictionary(wFile):
-	words = []
-	with open(wFile, 'r') as f:
-		allwords = f.readlines()
-	for x in allwords:
-    		x = x.rstrip()
-    		words.append(x)
+    words = []
+    with open(wFile, 'r') as f:
+        allwords = f.readlines()
+    for x in allwords:
+        x = x.rstrip()
+        words.append(x)
+    # Words are less then or equal to 9 and uncapitalised
+    words = [word for word in words if len(word) <= 9 and words[0].islower()]
+    # Convert list of words to set to ensure no duplicates
+    words = set(words)
+    return words
 
-	words = [word for word in words if len(word) <= 9 ]
-
-	# Convert list of words to set to ensure no duplicates
-	words = set(words)
-	return words
 def getDictionary():
     if (len(dictionary) <= 0):
         dictionary.update(genDictionary('wordlist.txt'))
