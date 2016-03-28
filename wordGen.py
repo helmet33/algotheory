ec19040a0a5778ce9d01927074e9b24e77312a85
Garrett Jordan <garrettjordan@Garretts-MacBook-Pro.local>
2016-03-28 01:33:17 +0100

    Operate on a copy of the list
    Operate on a copy of the letters pool both to avoid regeneration of
    distribution and to prevent the emptying of the main list between plays.

--- wordGen.py
+++ wordGen.py
@@ -30,9 +30,12 @@ def genList():
 # Select vowels and consonants, remove from letters pool shuffle and pop remaining letters return word. 
 # Split to methods and import to runner
 # random.shuffle(letters)
+
 lettersG = genList()
 # TEST CODE - test method for import - a known known
 def getLetters():
+    # Neccessary to copy list to avoid it emptying and also 
+    # to avoid regeneration
     letters = list(lettersG)
     return letters
 
@@ -46,7 +49,7 @@ def getConsnants(letPool):
     return c
 
 def genWord():
-    lets = genList()
+    lets = list(lettersG)
     word = []
     for i in range (0,3):
         v = random.choice(vowels)
