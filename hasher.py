7be480a4a1113ffad9c93fbae8111c7170f8cbbf
helmet33 <garrettjor@gmail.com>
2016-03-30 14:19:52 +0100

    Added hash lookup
    Added a hash lookup function. Will retrofit with existing file module.
    Like one done in class.

---
+++ hasher.py
@@ -0,0 +1,51 @@
+# Module to do hash look ups
+# Just as a way of comparison.
+# I expect this to be faster but 
+# will not be consistent across iterations
+
+a = ['peter', 'paul', 'john', 'mary', 'yarm']
+
+mapper = dict()
+def encoder(a):
+    for word in a:
+        sortw = sorted(word)
+        sortw = "".join(sortw)
+        hashedw = hash(sortw)
+        words = mapper.get(hashedw, set())
+        words.update({word})
+        mapper[hashedw] = words
+
+
+
+
+def checker(wrd):
+    srtwrd = sorted(wrd)
+    srtwrd = "".join(srtwrd)
+    hshwrd = hash(srtwrd)
+    results = mapper.get(hshwrd, None)
+    return results
+
+# Test code
+def  ifExist(x):
+    if x:
+        return len(x)
+    else:
+        return 0
+
+
+encoder(a)
+b = ['john', 'james', 'joe', 'mary']
+for k in b:
+    x = checker(k)
+    print("{0}: {1} :len {2}".format(k, x, ifExist(x)))
+
+
+
+demi = checker("seamus")
+
+print ("%s" % demi)
+
+
+
+
+    
